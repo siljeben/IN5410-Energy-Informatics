17041d321d6e8ce2ed29cdb1fdbed2f692fef946
David <gekler71@gmail.com>
2024-02-22 13:05:05 +0100

    added random pricing + plot

--- Assignment1/question1.py
+++ Assignment1/question1.py
@@ -1,11 +1,18 @@
 import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
 
 
 # timeslots for pricing
-pricing = ((range(0, 17), 0.5), (range(17, 20), 1), (range(20, 24), 0.5))
+pricing = np.zeros(24)
+pricing[0:17] = np.random.uniform(0.45, 0.65, 17)
+pricing[17:20] = np.random.uniform(0.75, 1.0, 3)
+pricing[20:24] = np.random.uniform(0.45, 0.65, 4)
 
-data = pd.read_csv("datafile.csv")
 
-
-
-
+pricing_plot = np.zeros(48)
+pricing_plot[::2] = pricing
+pricing_plot[1::2] = pricing
+plt.plot(np.arange(48)/2, pricing_plot)
+plt.ylim(0, 1)
+plt.show()
