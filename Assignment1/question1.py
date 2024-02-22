1d911bf6addb8ce1b1001bd63814d53d38d34083
peterhpo <peter.hjelle.petersenoeverleir@gmail.com>
2024-02-22 13:05:46 +0100

    expanded the pricing

--- Assignment1/question1.py
+++ Assignment1/question1.py
@@ -4,7 +4,16 @@ import pandas as pd
 # timeslots for pricing
 pricing = ((range(0, 17), 0.5), (range(17, 20), 1), (range(20, 24), 0.5))
 
-data = pd.read_csv("datafile.csv")
+expanded_pricing = []
+for r, p in pricing:
+    for _ in r:  # Use _ as a throwaway variable to indicate that the hour is not being saved
+        expanded_pricing.append(p)
+
+df = pd.DataFrame(expanded_pricing, columns=['price'])
+print(df)
+
+
+# data = pd.read_csv("datafile.csv")
 
 
 
