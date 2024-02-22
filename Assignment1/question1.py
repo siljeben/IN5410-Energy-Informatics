92edc4bd1d487547f16b948dc6312c520dc88840
David <gekler71@gmail.com>
2024-02-22 13:19:12 +0100

    switch back to ToU pricing for q1

--- Assignment1/question1.py
+++ Assignment1/question1.py
@@ -12,14 +12,14 @@ from matplotlib import pyplot as plt
 
 # timeslots for pricing
 pricing = np.zeros(24)
-pricing[0:17] = np.random.uniform(0.45, 0.65, 17)
-pricing[17:20] = np.random.uniform(0.75, 1.0, 3)
-pricing[20:24] = np.random.uniform(0.45, 0.65, 4)
+pricing[0:17] = 0.5
+pricing[17:20] = 1.0
+pricing[20:24] = 0.5
 
 
 pricing_plot = np.zeros(48)
 pricing_plot[::2] = pricing
 pricing_plot[1::2] = pricing
 plt.plot(np.arange(48)/2, pricing_plot)
-plt.ylim(0, 1)
+plt.ylim(0, 1.3)
 plt.show()
