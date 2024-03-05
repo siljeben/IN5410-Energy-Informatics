e5ed3c8431d87b0765cccb6c445afb9048e60693
peterhpo <peter.hjelle.petersenoeverleir@gmail.com>
2024-03-05 19:41:58 +0100

    Update question3.py

--- Assignment1/question3.py
+++ Assignment1/question3.py
@@ -8,7 +8,7 @@ pricing = RTP_pricing()
 
 data = pd.read_csv("datafile.csv")
 
-neighborhood = Neighborhood("Neighborhood 1", 30, "RTP", 1)
+neighborhood = Neighborhood(name="Neighborhood 1", households=30, pricing="RTP", peak_load=0)
 
 # TODO: Compute the best strategy...
 
