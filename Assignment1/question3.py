d53fef68049c385475cc8bb2a6630745040a1486
peterhpo <peter.hjelle.petersenoeverleir@gmail.com>
2024-03-05 19:41:13 +0100

    Update question3.py

--- Assignment1/question3.py
+++ Assignment1/question3.py
@@ -1,11 +1,16 @@
 import pandas as pd
 from question2 import RTP_pricing
 
+from classes import Neighborhood
+
 
 pricing = RTP_pricing()
 
 data = pd.read_csv("datafile.csv")
 
+neighborhood = Neighborhood("Neighborhood 1", 30, "RTP", 1)
+
+# TODO: Compute the best strategy...
 
 
 
