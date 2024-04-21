2aad2b0dcbd8421e1b36b031069d16bbb35bd60a
gekeleda <gekeler71@gmail.com>
2024-04-21 14:05:28 +0200

    remove imputing task 2, add timestamp to year decimal function

--- Assignment2/data_processing.py
+++ Assignment2/data_processing.py
@@ -1,14 +1,14 @@
-import pandas as pd
 import numpy as np
-
-def polar_angle(x, y):
-    angle = np.arctan2(y,x)
-    angle[angle<0] = 2*np.pi + angle[angle<0]
-    return angle
+from datetime import datetime
+from PyAstronomy import pyasl
 
 def components_to_angle(df):    
-    df["A10"] = polar_angle(df["U10"], df["V10"])
-    df["A100"] = polar_angle(df["U100"], df["V100"])
+    df["A10"] = np.arctan2(df["U10"], df["V10"])
+    df["A100"] = np.arctan2(df["U100"], df["V100"])
     df.drop(['U10', 'V10', 'U100', 'V100'], axis=1)
     return df
 
+def year_decimal_from_timestamp(timestamp: str):
+    format_string = "%Y%m%d %H:%M"
+    datetime_obj = datetime.strptime(timestamp, format_string)
+    return pyasl.decimalYear(datetime_obj)
